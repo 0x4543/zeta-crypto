0f3d586ee4c1c0caf17c264746d7802fd15f93cf
0x4543 <intodeadsilence@gmail.com>
2025-11-14 21:03:49 -0500

    Add build_info() function

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,16 @@ pub mod signer;
 pub mod wallet;
 pub mod walletconnect;
 
+pub use cli_utils::*;
 pub use config::ZetaConfig;
 pub use key_derivation::{derive_key_hkdf, derive_key_pbkdf2};
 pub use mnemonic::MnemonicHelper;
 pub use signer::Signer;
 pub use wallet::Wallet;
 pub use walletconnect::WalletConnectSession;
+
+pub fn build_info() -> String {
+    let version = env!("CARGO_PKG_VERSION");
+    let target = format!("{} {}", std::env::consts::OS, std::env::consts::ARCH);
+    format!("zeta-crypto {} | target {}", version, target)
+}
