52ed970deb8ea8a689b5eb0a022d967f2d0ff986
0x4543 <intodeadsilence@gmail.com>
2025-12-04 14:23:37 -0500

    refactor: convert MnemonicHelper to standalone functions

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -1,21 +1,17 @@
 use bip39::{Language, Mnemonic};
 use rand::RngCore;
 
-pub struct MnemonicHelper;
+pub fn generate() -> Mnemonic {
+    let mut entropy = [0u8; 16];
+    rand::thread_rng().fill_bytes(&mut entropy);
+    Mnemonic::from_entropy(&entropy).expect("entropy length ok")
+}
 
-impl MnemonicHelper {
-    pub fn generate() -> Mnemonic {
-        let mut entropy = [0u8; 16];
-        rand::thread_rng().fill_bytes(&mut entropy);
-        Mnemonic::from_entropy(&entropy).expect("entropy length ok")
-    }
+pub fn from_phrase(phrase: &str) -> anyhow::Result<Mnemonic> {
+    Ok(Mnemonic::parse_in_normalized(Language::English, phrase)?)
+}
 
-    pub fn from_phrase(phrase: &str) -> anyhow::Result<Mnemonic> {
-        Ok(Mnemonic::parse_in_normalized(Language::English, phrase)?)
-    }
-
-    pub fn to_seed(mnemonic: &Mnemonic, passphrase: &str) -> Vec<u8> {
-        let seed = mnemonic.to_seed_normalized(passphrase);
-        seed.to_vec()
-    }
+pub fn to_seed(mnemonic: &Mnemonic, passphrase: &str) -> Vec<u8> {
+    let seed = mnemonic.to_seed_normalized(passphrase);
+    seed.to_vec()
 }
