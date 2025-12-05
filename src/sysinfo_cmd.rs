f0db21eb89dcf4a5aef7e17c869de09f6606cfcf
0x4543 <intodeadsilence@gmail.com>
2025-12-05 17:27:32 -0500

    refactor: inline version printing logic into sysinfo command

--- src/sysinfo_cmd.rs
+++ src/sysinfo_cmd.rs
@@ -1,11 +1,22 @@
-use crate::version;
 use crate::ZetaConfig;
 use anyhow::Result;
 use std::io::{self, Write};
 use std::process::Command;
 
 pub fn handle_version() -> Result<()> {
-    version::print_version_info();
+    let rustc = Command::new("rustc")
+        .arg("--version")
+        .output()
+        .map(|o| String::from_utf8_lossy(&o.stdout).to_string())
+        .unwrap_or_else(|_| "unknown".into());
+
+    println!("Zeta Crypto CLI {}", env!("CARGO_PKG_VERSION"));
+    println!("Rust compiler: {}", rustc.trim());
+    println!(
+        "Platform: {} {}",
+        std::env::consts::OS,
+        std::env::consts::ARCH
+    );
     Ok(())
 }
 
